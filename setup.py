ee1aaff830bd8b3768365fecf78d4d6cec6747af
Akhil <55911943+nvakhilnair@users.noreply.github.com>
2020-06-23 16:58:32 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,26 +7,23 @@ def readme():
 
 
 setup(
-    name="covid123",
+    name="Covid-19-India-Graphical-Analysis",
     version="1.0.0",
     description="Gui application is used for Analysis of Covid-19 in India Graphicaly",
     long_description=readme(),
     long_description_content_type="text/markdown",
     url="https://github.com/nvakhilnair/",
     author="Akhil",
-    author_email="nvakhilnair@gmail.com",
+    author_email="developmentuse009r@gmail.com",
     license="MIT",
     classifiers=[
         "License :: OSI Approved :: MIT License",
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.7",
     ],
-    packages=['pythonfiles'],
-    include_package_data=True,
+    packages=[],
+    include_package_data=False,
     install_requires=["PyQt4>=4.11.4","pandas>=1.0.3"],
     entry_points={
-        "gui_scripts": [
-            "covid123=pythonfiles.run:fnc",
-        ]
     },
 )
